a7d8420721ecb5ec598c75faacf497ffac34a89a
Pushkaraj <pushkaraj4444@gmail.com>
2024-02-13 22:13:10 +0530

    Changes

--- server/mongodb/models/user.js
+++ server/mongodb/models/user.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    avatar: { type: String, required: true },
-    allProperties: [{ type: mongoose.Schema.Types.ObjectId, ref: "Property" }],
+    allDocuments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Documents" }],
 });
 
 const userModel = mongoose.model("User", UserSchema);
